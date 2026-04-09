fdd94018dc511b5390be483b10dda0197f4930d2
yosorkhaled <157757934+yosorkhaled@users.noreply.github.com>
2026-04-09 03:00:24 +0300

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 st.title("📚 Learning Weakness Analytics")
-st.markdown("### Week 1 — PDF Parsing & Data Pipeline")
+st.markdown("### PDF Parsing & Data Pipeline")
 st.markdown("---")
 
 def clean_text(text: str) -> str:
